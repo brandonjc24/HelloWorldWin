166bdf8a56f2bc4c07221b7df1261b886128b05a
Brandon Coles <brandonjc24@gmail.com>
2021-08-09 19:24:38 -0400

    Added button wrappers

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,10 +1,19 @@
 #include "MainComponent.h"
 
+
+
+
+
 //==============================================================================
 MainComponent::MainComponent()
 {
-    addAndMakeVisible(toggleButton);
+    heapButton.reset( new HeapButtonWrapper<juce::TextButton>(  []() {DBG("You clicked the heap"); },
+                                                                 new juce::TextButton("heapButton") 
+                                                             )
+                    );
 
+    addAndMakeVisible(button);
+    addAndMakeVisible( *heapButton );
     setSize (600, 400);
 }
 
@@ -28,5 +37,9 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
-    toggleButton.setBounds(10, 10, 30, 30);
+    // toggleButton.setBounds(10, 10, 30, 30);
+    button->setBounds( 498,
+                        10, 
+                        100, 30);
+    (*heapButton)->setBounds(200, 10, 100, 30);
 }
